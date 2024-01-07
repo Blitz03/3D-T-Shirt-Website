45b9e92af1566fc09918a1cfba84db383827c349
Mustafa <mrmrmustafag77@gmail.com>
2024-01-07 17:28:26 +0300

    Fix limit property

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
@@ -12,19 +12,19 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
-router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
-})
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+});
 
-router.route('/').post(async (req, res) => {
+router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
     const response = await openai.createImage({
       prompt,
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json'
+      size: "1024x1024",
+      response_format: "b64_json",
     });
 
     const image = response.data.data[0].b64_json;
@@ -32,8 +32,8 @@ router.route('/').post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong" })
+    res.status(500).json({ message: "Something went wrong" });
   }
-})
+});
 
 export default router;
